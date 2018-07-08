98484b5370d9a20c0fe92a6071d8b708b0af9dc1
Florian Saumurot <fsaumurot@skillbase.fr>
2018-07-08 18:10:58 +0200

    test webhooks

--- routes/stripeWebhook.js
+++ routes/stripeWebhook.js
@@ -1,11 +1,11 @@
 const express = require('express'),
     path = require('path'),
-    router = express.Router(),
-    stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
-
-const database = require('../services/database.js');
+    router = express.Router()
+    //stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
 
 router.post('/', function (req, res, next) {
+
+    console.log(req.body)
     res.json({
         status: req.body.status
     })
